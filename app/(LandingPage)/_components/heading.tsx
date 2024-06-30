fc9f92b8cf5f48c8f164846814bd2155b028fada
Aditya <prakaashaditya@gmail.com>
2024-06-30 15:28:56 +0530

    chore: Update landing page layout and styles

--- app/(LandingPage)/_components/heading.tsx
+++ app/(LandingPage)/_components/heading.tsx
@@ -9,14 +9,14 @@ const Heading = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <div className="max-w-3xl space-y-4">
+    <div className="max-w-3xl space-y-4 ">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Empowering Health, Securely. Welcome to KeepSafe.
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Revolutionizing Healthcare with Blockchain for Privacy and Trust
       </h3>
-      <Button asChild>
+      <Button asChild className="">
         <Link href="/auth">
           Enter
           <ArrowRight className="h-4 w-4 ml-2" />
--- app/(LandingPage)/page.tsx
+++ app/(LandingPage)/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
     <div className="min-h-full flex flex-col">
       <div className="flex flex-col items-center justify-center md:justify-start text-center flex-1 px-6 pb-20">
         <Heading />
-        <Heros />
+        {/* <Heros /> */}
       </div>
     </div>
   );
